92f4dce456d26e378a844cb10c1fe5e939dea470
Benjamín <benjamin.diaze@outlook.com>
2025-02-09 13:51:38 -0300

    Se añade authentication con Cognito y endpoint de prueba

--- MateoAPI/Program.cs
+++ MateoAPI/Program.cs
@@ -1,8 +1,37 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+string cognitoAppClientId = Environment.GetEnvironmentVariable("COGNITO_APP_CLIENT_ID") ?? throw new ArgumentNullException("COGNITO_APP_CLIENT_ID");
+string cognitoUserPoolId = Environment.GetEnvironmentVariable("COGNITO_USER_POOL_ID") ?? throw new ArgumentNullException("COGNITO_USER_POOL_ID");
+string cognitoRegion = Environment.GetEnvironmentVariable("COGNITO_REGION") ?? throw new ArgumentNullException("COGNITO_REGION");
+string[] allowedDomains = Environment.GetEnvironmentVariable("ALLOWED_DOMAINS")?.Split(",") ?? throw new ArgumentNullException("ALLOWED_DOMAINS");
+
 
 builder.Services.AddControllers();
+
+builder.Services.AddAWSLambdaHosting(LambdaEventSource.RestApi);
+
+builder.Services.AddCors(item => {
+    item.AddPolicy("CORSPolicy", builder => {
+        builder.WithOrigins(allowedDomains)
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
+
+builder.Services
+    .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, options => {
+        options.Authority = $"https://cognito-idp.{cognitoRegion}.amazonaws.com/{cognitoUserPoolId}";
+        options.TokenValidationParameters = new TokenValidationParameters {
+            ValidateAudience = true,
+            ValidAudience = cognitoAppClientId,
+            ValidateLifetime = true
+        };
+    });
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -17,6 +46,10 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors("CORSPolicy");
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
