0c2952ece88f2fe554eb023d0684f65726ae871b
Benjamín <benjamin.diaze@outlook.com>
2025-03-08 17:44:14 -0300

    Se elimina chmod, y se cambia UseShellExecute para ProcessStartInfo

--- InitialCreationLambda/InitialCreationLambda/Function.cs
+++ InitialCreationLambda/InitialCreationLambda/Function.cs
@@ -104,7 +104,7 @@ public class Function {
                 Arguments = $"--connection \"{connectionString}\"",
                 RedirectStandardOutput = true,
                 RedirectStandardError = true,
-                UseShellExecute = true,
+                UseShellExecute = false,
                 CreateNoWindow = true,
                 
             },
